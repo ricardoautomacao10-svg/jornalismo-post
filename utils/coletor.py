29c1658b3fd2b907413a460dcb326ef90ffe0052
ricardoautomacao10-svg <ricardo.automacao10@gmail.com>
2025-08-21 19:17:22 -0300

    Update coletor.py

--- utils/coletor.py
+++ utils/coletor.py
@@ -4,7 +4,9 @@ def extrair_noticia(url):
     artigo = Article(url, language='pt')
     artigo.download()
     artigo.parse()
+
     titulo = artigo.title
     texto = artigo.text
     imagem = artigo.top_image if artigo.top_image else ""
+
     return titulo, texto, imagem
