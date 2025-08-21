a3407baaaec981079b78c60d7712556b128f43c4
ricardoautomacao10-svg <ricardo.automacao10@gmail.com>
2025-08-21 16:08:56 -0300

    Update coletor.py

--- utils/coletor.py
+++ utils/coletor.py
@@ -1,12 +1,12 @@
 from newspaper import Article
 
 def extrair_noticia(url):
-    artigo = Article(url, language='pt')
+    artigo = Article(url, language="pt")
     artigo.download()
     artigo.parse()
 
-    titulo = artigo.title or "Sem título"
-    texto = artigo.text or "Sem conteúdo"
+    titulo = artigo.title
+    texto = artigo.text
     imagem = artigo.top_image if artigo.top_image else ""
 
     return titulo, texto, imagem
