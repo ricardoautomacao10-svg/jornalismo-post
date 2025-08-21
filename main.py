58d2c9405f3042690e4189455e0bc1b4ef721c04
ricardoautomacao10-svg <ricardo.automacao10@gmail.com>
2025-08-21 14:44:11 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
+from utils.coletor import extrair_noticia
 from utils.gerador import gerar_conteudo_completo
 
-if __name__ == "__main__":
-    url = "https://www.ubatuba.sp.gov.br/noticias/estudantesiniciam06ago/"
-    print("🔍 Buscando notícias no Google News...")
-    print(f"📰 Coletando: {url}")
-    try:
-        texto = gerar_conteudo_completo(url)
-        print("✅ Texto gerado:\n")
-        print(texto)
-    except Exception as e:
-        print(f"⚠ Erro ao processar: {e}")
+url = "https://www.ubatuba.sp.gov.br/noticias/estudantesiniciam06ago/"
+
+print("🔍 Buscando notícias no Google News...")
+print(f"📰 Coletando: {url}")
+
+titulo, conteudo = extrair_noticia(url)
+
+if conteudo:
+    print("✅ Extração concluída. Gerando conteúdo...")
+    texto_final = gerar_conteudo_completo(titulo, conteudo)
+    print("✅ Texto gerado:")
+    print(texto_final)
+else:
+    print("❌ Falha na extração de conteúdo.")
